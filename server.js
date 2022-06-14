a8caf17ea0f3c7c6d1d0ba4f8f6d1869f10a720b
gitkraken303 <johnspaxxvile@gmail.com>
2022-06-14 21:12:00 +0300

    make it better

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ dotenv.config();
 connectDatabase();
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // API
 app.use("/api/import", ImportData);
